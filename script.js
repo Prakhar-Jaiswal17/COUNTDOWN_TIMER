613c9420275bf4c7c1a95404f04e02d528e58308
Prakhar Jaiswal <pranun.17@gmail.com>
2025-12-12 20:53:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ const minute= document.getElementById('minute');
 const second= document.getElementById('second');
 
 const btn=document.getElementById("subBtn");
+const rstBtn=document.getElementById("reset");
+const eventHeading=document.getElementById("Event");
+
+rstBtn.onclick=()=>{
+    location.href="#top";
+    document.getElementById("targetDate").value='';
+    document.getElementById("targetTime").value='';
+    document.getElementById("eventName").value='';
+    rstBtn.style.display="none";
+}
 
 let targetDate;
 let targetTime;
 let countTime;
-let eventName="Event Name";
+let eventName;
 
 const oneSecond=1000;
 const oneMinute=oneSecond*60;
 const oneHour=oneMinute*60;
 const oneDay=oneHour*24;
+
+let intervalId;
 
 btn.onclick=()=>{
     targetDate=document.getElementById("targetDate").value;
@@ -24,10 +36,12 @@ btn.onclick=()=>{
     }else{
         location.href="#Event"
     }
-    const eventHeading=document.getElementById("Event");
+    clearInterval(intervalId);
     eventHeading.innerHTML=eventName;
+    eventHeading.style.color="grey";
     countTime=new Date(targetDate+'T'+targetTime).getTime();
-    setInterval(timer,1000);
+    // timer();
+    intervalId=setInterval(timer,1000);
 };
 
 function addZero(x){
@@ -38,6 +52,10 @@ function timer(){
     let currentTime=new Date().getTime();
     let difference=countTime-currentTime;
     if(difference<0){
+        eventHeading.innerHTML="Countdown Over!!";
+        eventHeading.style.color="rgba(228, 228, 228, 1)";
+        rstBtn.style.display="block";
+        clearInterval(intervalId);
         return;
     }
 
